6bd9f946cb8fae0088ea6b7d92a25a01e42e1882
Ivan Chen <chenjh@Ivans-MacBook-Air.local>
2022-11-27 09:07:43 +0800

    expo-splash-screen

--- client/App.js
+++ client/App.js
@@ -1,10 +1,33 @@
+import { useState, useEffect, useCallback } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAppIsLoaded(true);
+    }, 3000)
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    await SplashScreen.hideAsync();
+  }, [appIsLoaded]);
+
+  if (!appIsLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaProvider style={styles.container}>
+    <SafeAreaProvider
+      style={styles.container}
+      onLayout={onLayout}
+    >
       <SafeAreaView>
         <View>
           <Text>Hi!</Text>
